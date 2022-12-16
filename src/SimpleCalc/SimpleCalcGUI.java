408f0cc09b77dffa851a36dd7c2eaf3599e676e7
Radclyf <shanksakagame66@gmail.com>
2022-12-16 12:00:09 +0800

    Add SimpleCalcGUI components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,10 +1,50 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI {
     private JPanel panel1;
     private JTextField textField1;
     private JComboBox comboBox1;
     private JButton computeResultButton;
+
+    public SimpleCalcGUI() {
+        JFrame frame = new JFrame("Simple Calculator");
+        frame.setContentPane(panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        computeResultButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double operand = Double.parseDouble(textField1.getText());
+                String operator = (String) comboBox1.getSelectedItem();
+                double result = 0;
+
+                switch(operator) {
+                    case "+":
+                        result = operand + operand;
+                        break;
+                    case "-":
+                        result = operand - operand;
+                        break;
+                    case "*":
+                        result = operand * operand;
+                        break;
+                    case "/":
+                        result = operand / operand;
+                        break;
+                }
+
+                JOptionPane.showMessageDialog(null, "Result: " + result);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new SimpleCalcGUI();
+    }
 }
