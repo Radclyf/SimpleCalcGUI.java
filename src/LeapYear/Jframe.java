910ded50d6086d7fc3b78c7ae7500d129fe4a10d
Radclyf <shanksakagame66@gmail.com>
2022-12-16 11:43:30 +0800

    Rename LeapYear components

---
+++ src/LeapYear/Jframe.java
@@ -0,0 +1,8 @@
+package LeapYear;
+
+import javax.swing.*;
+
+public class Jframe extends JFrame {
+    public Jframe(String leap_year_checker) {
+    }
+}
--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,52 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI {
     private JPanel panel1;
-    private JTextField tfYear;
-    private JButton btnCheckerYear;
+    private JTextField textField1;
+    private JButton checkButton;
+
+    public LeapYearGUI() {
+        JFrame frame = new Jframe("Leap Year Checker");
+        frame.setContentPane(panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        checkButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year = Integer.parseInt(textField1.getText());
+
+                if(isLeapYear(year)) {
+                    JOptionPane.showMessageDialog(null, "Leap Year");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Not a leap year");
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new LeapYearGUI();
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(year % 4 == 0){
+            if (year % 100 ==0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
