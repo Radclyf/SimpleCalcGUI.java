431ffcdcea555bf8f12fa26e28df0454a516270f
Radclyf <shanksakagame66@gmail.com>
2022-12-17 18:31:17 +0800

    Add SimpleCalcGUI.java main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,70 +1,55 @@
 package SimpleCalc;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
 
 public class SimpleCalcGUI {
     private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
-    private JLabel lblResult;
+    private JTextField tfNumber1;
+    private JTextField tfNumber2;
+    private JComboBox<String> cbOperations;
+    private JButton btnCompute;
+    private JTextField lblResult;
 
     public SimpleCalcGUI() {
-        // Initialize and set up the layout of the GUI components
-        textField1 = new JTextField(10);
-        textField2 = new JTextField(10);
-        String[] operatorSymbols = {"+", "-", "*", "/"};
-        comboBox1 = new JComboBox(operatorSymbols);
-        computeResultButton = new JButton("Compute Result");
-        lblResult = new JLabel("Result: ");
+        tfNumber1 = new JTextField();
+        tfNumber2 = new JTextField();
+        String[] operatorSymbol = {"+", "-", "*", "/"};
+        cbOperations = new JComboBox<>(operatorSymbol);
+        btnCompute = new JButton("Compute Result");
+        lblResult = new JTextField("");
 
-        panel1 = new JPanel();
-        panel1.add(textField1);
-        panel1.add(comboBox1);
-        panel1.add(textField2);
-        panel1.add(computeResultButton);
-        panel1.add(lblResult);
-
-        // Add an action listener to the computeResultButton
-        computeResultButton.addActionListener(new ActionListener() {
+        btnCompute.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Get the values from the text fields
-                double num1 = Double.parseDouble(textField1.getText());
-                double num2 = Double.parseDouble(textField2.getText());
+                int x = Integer.parseInt(tfNumber1.getText());
+                int y = Integer.parseInt(tfNumber2.getText());
+                String operator = (String) cbOperations.getSelectedItem();
+                int result = 0;
 
-                // Get the chosen operator
-                String operator = comboBox1.getSelectedItem().toString();
-
-                double result = 0;
-
-                // Perform the computation based on the chosen operator
                 switch (operator) {
                     case "+":
-                        result = num1 + num2;
+                        result = x + y;
                         break;
                     case "-":
-                        result = num1 - num2;
+                        result = x - y;
                         break;
                     case "*":
-                        result = num1 * num2;
+                        result = x * y;
                         break;
                     case "/":
-                        result = num1 / num2;
+                        result = x / y;
                         break;
                 }
 
-                // Display the result in the lblResult label
-                lblResult.setText(String.valueOf(result));
+                lblResult.setText(String.format("", result));
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("SimpleCalcGUI");
+        JFrame frame = new JFrame("Simple Calculator");
         frame.setContentPane(new SimpleCalcGUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
